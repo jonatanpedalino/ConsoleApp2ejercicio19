51b81559d23f1efb9c92f7801f69f70f1991d04e
pamela <pamela@AIWA_NA-1413>
2022-07-04 20:45:08 -0300

    Agregar archivos de proyecto.

---
+++ ConsoleApp2ejercicio19/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2ejercicio19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("ingrese el numero");
+            numero=int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+                Console.WriteLine($"{numero} es par");
+            else
+                Console.WriteLine($"{numero} es impar");
+            Console.ReadLine();
+        }
+    }
+}
